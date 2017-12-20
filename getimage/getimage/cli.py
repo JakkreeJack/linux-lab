f58073d8b8cc40702f9860705c4ea38a4d00b106
jakkree <jakkree.ko.59@ubu.ac.th>
2017-12-21 04:51:33 +0700

    Project

--- getimage/getimage/cli.py
+++ getimage/getimage/cli.py
@@ -1,10 +1,27 @@
 import click
+import requests
+import sys
 
+from PIL import Image
+try:
+    from StringIO import StringIO
+except: # when using python3.x
+    from io import BytesIO, StringIO
+from bs4 import BeautifulSoup
 
 @click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
-    """getimage"""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+@click.argument('src', nargs=-1)
+#@click.argument('dst', nargs=1)
+def main(src):
+    	"""Get Poster Kseries"""
+	name=""	
+	for fn in src:
+        	name+='%s' % (fn)+" "
+	#name+='%s'%(dst)
+	url='https://www.movie2free.com/{}'.format(name.replace(" ",""))
+	html=requests.get(url)
+	beau=BeautifulSoup(html.content,'html.parser')
+	poster=beau.find_all('div',{'class':'movie-thumbnail'})[0].img['src']
+	req=requests.get(poster)
+	img=Image.open(StringIO(req.content))
+	img.show()
